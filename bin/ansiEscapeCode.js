e9c83858354daa1c79fb8b002209961a5b33a44f
ljh255 <jinhai@carbonstop.net>
2023-08-08 11:40:28 +0800

    fix:删除无用代码

--- bin/ansiEscapeCode.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-console.log("\x1B[41m\x1B[4m%s\x1B[0m", "your name");
-console.log("\x1B[2B%s", "your name");
-console.log("\x1B[2G%s", "your name");
-console.log("\x1B[2F%s", "your name");
-
-console.log("\x1b[31mhellow carbonStop\x1b[39m");
-console.log("\u001b[31mhellow carbonStop\u001b[39m");
